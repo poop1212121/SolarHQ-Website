6d2024ec049ea169438e4cd93bc417589bda6fff
poop1212121 <solarmusic155@gmail.com>
2025-04-14 18:10:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const CLIENT_ID = "1361359933030793216";
-const REDIRECT_URI = "https://solar-hq.vercel.app/index.html";
+const REDIRECT_URI = "https://solar-hq.vercel.app/dashboard.html";
 const BOT_INVITE = `https://discord.com/oauth2/authorize?client_id=1361359933030793216&permissions=8&integration_type=0&scope=bot`;
 const DISCORD_OAUTH = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=identify guilds`;
 
